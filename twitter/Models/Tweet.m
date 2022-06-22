677ca606f1c4cabfc18b7e41f5e57fb3a424112a
Jacqueline Jou <jacquelinejou@fb.com>
2022-06-22 13:41:49 -0700

    Removed unused code that I missed before

--- twitter/Models/Tweet.m
+++ twitter/Models/Tweet.m
@@ -37,10 +37,7 @@
         // Format createdAt date string
         NSString *createdAtOriginalString = dictionary[@"created_at"];
         NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-        
         // Configure the input format to parse the date string
-        formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
-        
         NSDate *date = [formatter dateFromString:createdAtOriginalString];
         self.createdAtString = [date shortTimeAgoSinceNow];
     }
